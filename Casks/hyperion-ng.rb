4a520a798a24cfe38d251f8fabc8d78e8bebbd5a
Ryan Sheatsley <ryan@sheatsley.me>
2023-12-14 17:10:27 -0600

    add hyperion ng cask

---
+++ Casks/hyperion-ng.rb
@@ -0,0 +1,17 @@
+cask "hyperion-ng" do
+    version "2.0.15"
+    sha256 "34218afa326d72a90f67c1fa76fe77cc2710a55f99aabce66e5db606ee8ab365"
+
+    url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x86_64.dmg",
+        verified: "github.com/hyperion-project/hyperion.ng/"
+    name "Hyperion NG"
+    desc "The successor to Hyperion aka Hyperion Next Generation"
+    homepage "https://github.com/hyperion-project/hyperion.ng"
+
+    livecheck do
+        url :homepage
+        regex(%r{href=.*?/Hyperion-(\d+(?:\.\d+)*)\-macOS-x86_64.dmg}i)
+    end
+
+    app "Hyperion.app"
+end
