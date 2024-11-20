a7bf4ebe88b05ac2b3a5248b76b0b16c4010dda2
Ryan Sheatsley <ryan@sheatsley.me>
2024-11-19 20:26:18 -0600

    update hyperion-ng to 2.0.16

--- Casks/hyperion-ng.rb
+++ Casks/hyperion-ng.rb
@@ -1,5 +1,5 @@
 cask "hyperion-ng" do
-    version "2.0.15"
+    version "2.0.16"
     sha256 "34218afa326d72a90f67c1fa76fe77cc2710a55f99aabce66e5db606ee8ab365"
 
     url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x86_64.dmg",
