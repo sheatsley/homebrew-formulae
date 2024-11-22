ad88fc04bf9135e515e2765bfb878ab270d3bb48
Ryan Sheatsley <ryan@sheatsley.me>
2024-11-21 20:43:06 -0600

    typofix for hyperion 2.0.16 url

--- Casks/hyperion-ng.rb
+++ Casks/hyperion-ng.rb
@@ -2,7 +2,7 @@ cask "hyperion-ng" do
     version "2.0.16"
     sha256 "34218afa326d72a90f67c1fa76fe77cc2710a55f99aabce66e5db606ee8ab365"
 
-    url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x86_64.dmg",
+    url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-x64.dmg",
         verified: "github.com/hyperion-project/hyperion.ng/"
     name "Hyperion NG"
     desc "The successor to Hyperion aka Hyperion Next Generation"
